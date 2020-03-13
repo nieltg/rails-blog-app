12c88531e16ba2a84809743de31f05140e002573
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2020-03-13 14:42:20 +0700

    Fix route issue when using format like JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, path: '/'
+  resources :posts, path: '/posts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
